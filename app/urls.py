6ff9d0b2bfc41fa92d28447b91280813459f6940
AlzyWelzy <welzyalzy@gmail.com>
2024-02-24 21:10:54 +0530

    fixed some bugs

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-main_site = [
-    path("", index, name="index"),
-]
-
-urlpatterns = main_site
--- app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from datetime import datetime
-
-# Create your views here.
-
-
-def index(request):
-    context = {"year": datetime.now().year}
-    return render(request, "index.html", context)
